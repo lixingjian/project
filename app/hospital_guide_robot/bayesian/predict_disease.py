beb976842a54fbc4859762d860c381e60b5010d5
lixingjian <1762778193@qq.com>
2017-07-17 09:21:03 +0800

    bayesian update

--- app/hospital_guide_robot/bayesian/predict_disease.py
+++ app/hospital_guide_robot/bayesian/predict_disease.py
@@ -8,17 +8,30 @@ from pgmpy.inference import VariableElimination
 import json
 from pgmpy.readwrite import BIFReader, BIFWriter, XMLBIFReader, XMLBIFWriter, ProbModelXML, UAIReader, UAIWriter
 
+disease_part = {}
+disease_rate = {}
+for line in open('disease_intro.json').readlines():
+	js = json.loads(line.rstrip())['struct'] 
+	disease_rate[js['id']] = js['rate']
+	disease_part[js['id']] = js['department']
+
 fea_map = str_util.read_kv_file('models/feature.id')
+disease_id = {}
 disease_name = {}
 ds_rind = {}
 for line in open('disease_symptom.json').readlines():
 	js = json.loads(line.rstrip())
 	id = js['id']
+	if not id in disease_rate:
+		continue
 	disease_name[id] = js['name']
+	disease_id[js['name']] = id
 	for s, w in js['symptoms'].items():
 		if not s in ds_rind:
-			ds_rind[s] = set([])
-		ds_rind[s].add(id)	
+			ds_rind[s] = {} 
+		ds_rind[s][id] = disease_rate[id]
+
+print('init succeed ... type symptoms ...')
 
 while 1:
 	try:
@@ -26,13 +39,30 @@ while 1:
 	except:
 		break
 	a = buf.rstrip().split(' ')
-	ids = set([])
+	candidates = {} 
 	for sym in a:
+		if sym in disease_id:
+			id = disease_id[sym]
+			if not id in candidates:
+				candidates[id] = 0
+			candidates[id] += 1	
 		if sym in ds_rind:
-			ids = ids.union(ds_rind[sym])
-	
+			for id, rate in ds_rind[sym].items():
+				if not id in candidates:
+					candidates[id] = 0
+				candidates[id] += rate	
+	ids = sorted(candidates.items(), key=lambda d:d[1], reverse=True)[:20]
+	print('candidates generated')
+
 	dw = {}
-	for i in ids:
+	deps = {}
+	for i, r in ids:
+		if r >= 1:
+			for d in disease_part[i]:
+				if not d in deps:
+					deps[d] = 0
+				deps[d] += 1
+
 		try:
 			model = BIFReader('models/model.bif.%s' % i).get_model()
 		except:
@@ -51,11 +81,27 @@ while 1:
 			if not sym in fea_map:
 				continue
 			observed_info[fea_map[sym]] = 0
+			
 		score = infer.query(variables = [key], evidence = observed_info)[key].values[0]
 		dw[disease_name[i]] = score
-		print('%s: %s = %.8f' % (i, disease_name[i], score))	
+		#print('%s: %s = %.8f' % (i, disease_name[i], score))	
+
+		for d in disease_part[i]:
+			if not d in deps:
+				deps[d] = 0
+			deps[d] += score	
+	
+	if len(deps) == 0:
+		print('unknown symptoms...')
+		continue
 	
 	for name, score in sorted(dw.items(), key=lambda d:d[1], reverse=True)[:10]:
 		print('%s\t%.8f' % (name, score))
+	
+	dep_list = sorted(deps.items(), key=lambda d:d[1], reverse=True)
+	print('department: ')
+	for dep in dep_list[:3]:
+		print(dep)
+	
 	sys.stdout.flush()	
 
