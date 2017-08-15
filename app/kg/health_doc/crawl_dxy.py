64b9bafd49630d6add868733cb60d0b02791fca2
lixingjian <1762778193@qq.com>
2017-08-15 08:53:02 +0800

    update spider

--- app/kg/health_doc/crawl_dxy.py
+++ app/kg/health_doc/crawl_dxy.py
@@ -10,8 +10,15 @@ seeds = ['http://www.dxy.cn/res/medical/news_4',
         'http://surg.dxy.cn/tag/appendicitis',
         'http://www.dxy.cn/']
 white_list = [r'http://.+?\.dxy\.cn/\+?']
+black_list = [r'http://i\.dxy\.cn/.+?',
+              r'http://e\.dxy\.cn/.+?',
+              r'http://drugs\.dxy\.cn/.+?',
+              r'http://meeting\.dxy\.cn/.+?',
+              r'http://.+?\.dxy\.cn/.+?/bbs/.+?',
+              r'.+?\.png',
+              r'http://d\.dxy\.cn/.+?']
 download_list = [r'http://\w+.dxy.cn/article/\d+', r'http://\w+.dxy.cn/res/medical/*']
-sb = MiniSpider('%s/db' % retdir, '%s/page' % retdir, seeds, 
+sb = MiniSpider('%s/db' % retdir, '%s/page' % retdir, seeds, black_list = black_list,
 		white_list = white_list, download_list = download_list, thread_num = 1, time_sleep = 5,
 		queue_max_size = 1e6, url_max_size = 1e9, file_max_size = 1e12)
 
