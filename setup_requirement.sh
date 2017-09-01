c4a69badb150cb9af4ef1aab23ad59f6d3daf195
lixingjian <1762778193@qq.com>
2017-09-01 14:33:49 +0800

    add setup script

--- setup_requirement.sh
+++ setup_requirement.sh
@@ -3,6 +3,10 @@
 dir="/home/work/tools"
 mkdir -p $dir
 
+yum -y update
+rpm -Uvh http://ftp.iij.ad.jp/pub/linux/fedora/epel/6/x86_64/epel-release-6-8.noarch.rpm 
+rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+
 #install vim
 if [ ! -f $dir/vim-8.0.tar.bz2 ]; then
 	wget ftp://ftp.vim.org/pub/vim/unix/vim-8.0.tar.bz2 -P $dir
